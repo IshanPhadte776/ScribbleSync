cbb089481fb5a370a7ad2051fff3b95ff5783228
Ishan Phadte <ishanphadte@gmail.com>
2023-06-28 11:02:32 -0400

    Added ENV file

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 // Create Express app
 const app = express();
@@ -25,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-const uri = "mongodb+srv://<username>:<password>@<cluster-url>/database-name";
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error(error));
