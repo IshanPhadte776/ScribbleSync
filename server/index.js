56a8d074f7a4d156567891228b1cafd773439a54
Ishan Phadte <ishanphadte@gmail.com>
2023-07-11 19:19:28 -0400

    Changed Port

--- server/index.js
+++ server/index.js
@@ -270,7 +270,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
